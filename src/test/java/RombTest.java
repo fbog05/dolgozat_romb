a0f3b3c01cfcc41e0e85d91114135026f724d689
Ferencsik Boglárka <boglarkaferencsik05@gmail.com>
2024-01-10 10:23:52 +0100

    Teszt2

--- src/test/java/RombTest.java
+++ src/test/java/RombTest.java
@@ -1,6 +1,13 @@
+import org.testng.annotations.BeforeTest;
+
 import com.example.Rombusz;
 
 public class RombTest {
     
     Rombusz rombusz;
+
+    @BeforeTest
+    public void setup(){
+        this.rombusz = new Rombusz();
+    }
 }
